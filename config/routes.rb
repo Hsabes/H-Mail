81a1b9c0b5911eedbf47960c017291089d352613
Harrison Sabean <hsabes@Harrisons-iMac.local>
2022-10-25 16:58:23 -0400

    altered routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
-  resources :emails
-  resources :users
+  
 
   # SESSIONS
+
+  namespace :api do
+
+  resources :emails
+  resources :users
 
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   get "/me", to: "sessions#show"
 
+  end
+
+  get '*path',
+    to: 'fallback#index',
+    constraints: ->(req) { !req.xhr? && req.format.html? }
+
 end
-
-# Rails.application.routes.draw do
-#   get '*path',
-#       to: 'fallback#index',
-#       constraints: ->(req) { !req.xhr? && req.format.html? }
-# end
-
-# users/:user_id/emails
