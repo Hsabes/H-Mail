aab1fffcee52ae28ec99df790688605c7a27f52a
Harrison Sabean <hsabes@Harrisons-iMac.local>
2022-10-25 17:37:31 -0400

    commiting to change branches

--- db/migrate/20220810214500_create_users.rb
+++ db/migrate/20220810214500_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :email
       t.string :name
       t.string :username
-      t.string :profile_picture
+      t.string :avatar
       t.string :password_digest
 
       t.timestamps
